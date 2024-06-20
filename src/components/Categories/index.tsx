453f1ac4b34cf22151a1ac0e84cf5619cda270f7
Vinicius-Barbosa-Santos <viniciusfoxsantos@gmail.com>
2024-06-20 15:56:14 -0300

    feat: add Categories component

--- src/components/Categories/index.tsx
+++ src/components/Categories/index.tsx
@@ -1,9 +1,27 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { Category } from "../../types/category.types"
+import axios from "axios"
+import { env } from "../../config/env.config"
 
 export const Categories = () => {
 
     const [categories, setCategories] = useState<Category[]>([])
+
+    console.log(categories)
+
+    const fetchCategories = async () => {
+        try {
+            const { data } = await axios.get(`${env.apiUrl}/categories`)
+
+            setCategories(data)
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    useEffect(() => {
+        fetchCategories()
+    }, [])
 
     return (
         <div className="categories-container">
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,11 @@
+import { Categories } from "../../components/Categories"
 import { Header } from "../../components/Header"
 
 export const HomePage = () => {
   return (
     <div>
       <Header />
+      <Categories />
     </div>
   )
 }
