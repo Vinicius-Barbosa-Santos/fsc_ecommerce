3eda11fb3b3ec626bc23630d61c43fdedd638b3f
Vinicius-Barbosa-Santos <viniciusfoxsantos@gmail.com>
2024-08-20 12:08:54 -0300

    feat: add empty message to cart

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -17,7 +17,7 @@ export const Header = () => {
     const navigate = useNavigate()
 
     const { isAuthenticated } = useContext(UserContext)
-    const { toggleCart } = useContext(CartContext)
+    const { toggleCart, productsCount } = useContext(CartContext)
 
     const handleHomePage = () => {
         navigate('/')
@@ -50,7 +50,7 @@ export const Header = () => {
                 <HeaderItem onClick={() => signOut(auth)}>Sair</HeaderItem>
                 <HeaderItem onClick={toggleCart}>
                     <BsCart3 size={25} />
-                    <p style={{ marginLeft: 5 }}>5</p>
+                    <p style={{ marginLeft: 5 }}>{productsCount}</p>
                 </HeaderItem>
             </HeaderItems>
         </HeaderContainer>
